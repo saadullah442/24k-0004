e1593e2c89afea3af02bb8b47542c51bada8e795
Saad Ullah <83773413+saadullah442@users.noreply.github.com>
2024-09-10 11:10:38 +0500

    Lab04 Tasks

---
+++ task3.c
@@ -0,0 +1,34 @@
+#incude <stdio.h>
+int main() {
+
+  char charac;
+  printf("Enter a character");
+  scanf("%c", charac);
+  
+
+
+  void Checckdigit() {
+    
+    if (charac/charac) {
+      printf("It is digit");
+    }  
+
+  }
+
+  void Checkvowel() {
+    
+
+    if(charac == 'a' || charac == 'e' || charac == 'i' || charac == 'o' || charac == 'u' || charac == 'A' ||                 charac == 'E' || charac == 'I' || charac == 'O' || charac == 'U') {
+     
+       printf("Is a vowel");
+
+    }
+    
+    
+
+
+  }  
+
+
+
+}
