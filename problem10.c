82f38262397b4a807083a87a55a3035005fa837b
Saad Ullah <83773413+saadullah442@users.noreply.github.com>
2024-09-15 16:49:32 +0500

    Prolem10.c PF theory updated

--- problem10.c
+++ problem10.c
@@ -3,12 +3,13 @@
 int main() {
 
 
-  int numBeforeZero, number,count,removedNumber, powerCount,   remNumCount, zeroCount;
+  int numOfNine, numBeforeZero, number,count,removedNumber, powerCount,   remNumCount, zeroCount;
   int numCopy, numBefore;
 
   printf("Enter a number: ");
   scanf("%d", &number); 
-
+  
+  numOfNine = 0;
   numCopy = 0;
   numBefore = 0;
   count = 0;
@@ -16,64 +17,102 @@ int main() {
   zeroCount = 0;
   remNumCount = 0;
   removedNumber = 0;
-  while (count != 9) {
+
+  numCopy = number;
+  while(numCopy != 0) {  
+   
     count = 0;
-    while (number % 10 != 0) {
-      number = number - 1;
+    while (numCopy % 10 != 0) {
+      numCopy = numCopy - 1;
       count = count + 1;
     }
-    printf("Value of count: %d\n", count);
-    printf("Value of number: %d\n", number);
-    numBefore = 0;
-    if (count != 9) { 
-      if (count == 0) {
+   
+    numCopy = numCopy / 10;
+     
+    if(count == 9) {
+      numOfNine = numOfNine + 1;
 
-        numCopy = number;
-        numCopy = numCopy / 10;
-        while (numCopy % 10 != 0) {
-          numCopy = numCopy - 1;
-          numBefore = numBefore + 1;
-        }
-        printf("Value of numBefore: %d\n", numBefore);
-        if (numBefore != 9) {
-          printf("Incremented zero count\n");
-          zeroCount = zeroCount + 1;
-        }
-        
-        // zeroCount = zeroCount + 1;
-
-      }
-      number = number / 10;
-      printf("value of zeroCount: %d\n", zeroCount);
-      printf("value of powerCount: %d\n", powerCount);
-      removedNumber = removedNumber + ( count * ( (int)(pow(10,powerCount) +1e-9) ) );
-      printf("value of removedNumber: %d\n", removedNumber);
-      if (numBefore != 9) {
-        remNumCount = remNumCount + 1;
-      }
-      if (count == 0) {
-        powerCount = powerCount + zeroCount;
-      } else {
-        powerCount = powerCount + 1;
-      }       
-      zeroCount = 0;
-    }  
-    
-    else if (count == 9) { 
-      number = number + count;
-      printf("IN COUNT == 9\n");
-      printf("value of remNumCount: %d\n", remNumCount);
-      printf("Last value of removedNumber: %d\n", removedNumber);
-      printf("value of number: %d\n", number);
-
-      //  (int)(pow(5, 2)+1e-9)
-      printf("Printing power: %d\n", (int)(pow(10, remNumCount) +1e-9));
-      printf("Last value of zeroCount: %d\n", zeroCount);
-      number = (number *  (int)(pow(10, remNumCount) +1e-9) ) + removedNumber;
     }
+       
+  }//while ended
   
-  }
-  printf("%d", number);
+  numCopy = 0;
+  count = 0;
+  while (numOfNine != 0) {
 
+    while (count != 9) {
+      count = 0;
+      while (number % 10 != 0) {
+        number = number - 1;
+        count = count + 1;
+      }// while ended
+    
+      numBefore = 0;
+      if (count != 9) { 
+        if (count == 0) {
+          numCopy = number;
+          numCopy = numCopy / 10;
+          while (numCopy % 10 != 0) {
+            numCopy = numCopy - 1;
+            numBefore = numBefore + 1;
+          }// while ended
+       
+          if (numBefore != 9) {
+         
+            zeroCount = zeroCount + 1;
+          }// ifended
+
+        }// if count == 0 ended
+        number = number / 10;  
+        printf("value of number after division: %d\n", number);  
+        removedNumber = removedNumber + ( count * ( (int)(pow(10,powerCount) +1e-9) ) );
+
+        if (numBefore != 9) {
+          remNumCount = remNumCount + 1;
+        }
+
+        if (count == 0) {
+          powerCount = powerCount + zeroCount;
+        } else {
+          powerCount = powerCount + 1;
+        }       
+        
+        zeroCount = 0;
+      }// if count != 9 ended
+      else if( count == 9 && numOfNine != 1) {
+        number = number/10;
+        removedNumber = removedNumber + ( count * ( (int)(pow(10,powerCount) +1e-9) ) );
+        powerCount = powerCount + 1;
+        remNumCount = remNumCount + 1;
+        
+  
+        printf("value of number after division: %d\n", number);
+      }
+      else if (count == 9 && numOfNine == 1) { 
+        number = number + count;
+        printf("number * 10Expo: %d\n", (int)(pow(10, remNumCount) +1e-9));
+        number = (number *  (int)(pow(10, remNumCount) +1e-9) ) + removedNumber;
+      }
+       
+       printf("numOfNine: %d\n", numOfNine);
+
+       printf("Value of count: %d\n", count);
+       printf("value of zeroCount: %d\n", zeroCount);
+       printf("value of powerCount: %d\n", powerCount);
+       printf("value of remNumCount: %d\n", remNumCount);
+      
+       
+      
+       printf("value of number after adding: %d\n", number);
+       printf("value of removedNumber: %d\n", removedNumber);
+
+    }// while count != 9
+    count = 0;
+    numOfNine = numOfNine - 1;
+    printf("%d\n", number);
+    //printf("numOfNine in while != 0: %d\n", numOfNine);
+  }// while numOfNine != 0
+
+  printf("last number: %d", number);
 
 }
