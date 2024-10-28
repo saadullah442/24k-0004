d4a543462459ca1b7b1101e950ab686599ba6219
Saad Ullah <83773413+saadullah442@users.noreply.github.com>
2024-10-28 23:23:28 +0500

    Add files via upload

--- Theory Assignments/Assignment2/Task3.c
+++ Theory Assignments/Assignment2/Task3.c
@@ -2,20 +2,20 @@
 Name: Saad ullah
 Section: BAI-1A
 Roll-num: 24k-0004
-Desc: Removing redundent characters from string
+Desc: Removing redundent letters from string
 */
 
 #include <string.h>
 #include <stdio.h>
 #include <limits.h>
 
-
 int main() {
 
   char word[30];
+  
   int start = -1; 
   int end = -1;
-  int i,j,k;
+  int i,j,k,l;
   int size;
  
   printf("Enter word: ");
@@ -23,12 +23,14 @@ int main() {
 
   size = strlen(word);
 
+  char trunc_word[size];
+
   // going through each characters
   for(i=0;i<size;i++){
    start = -1;
    end = -1;
 
-   // start index of recurring character
+   // calculating start index of recurring character
    if(word[i] == word[i + 1] ) {
       start = i; 
       
@@ -37,24 +39,33 @@ int main() {
         if(word[j] == word[i]) end = j; 
       }// end loop j
     }// end if
-
-    printf("Start index: %d\n", start);
-    printf("End index: %d\n", end); 
  
-    // Do not remove anything if redundancy not found
+    // Do not remove anything if redundancy not found and go to next letter
     if (start == -1 || end == -1) continue;
     
-    // removing redundant characters
-    for(k=start;k<=end;k++){
-      word[k] = '\0';
-      printf("Word after removing a character: %s\n", word);
-    }// end loop k
-    printf("Shortened word is: %s\n", word);
+
+    // removing redundant letters
+    for(k=0;k<start;k++){
+       trunc_word[k] = word[k];
+    }// end for loop k
+   
+    l = k;
+    k=end;  
+
+    while(k < size){
+      trunc_word[l] = word[k];
+      l++;
+      k++;  
+    }// end while
+    
+    // overwriting the redundant word with non-redundant word
+    for(k=0;k<size;k++){
+      word[k] = trunc_word[k];
+    }  
+ 
 
   }// end loop i
 
-  
-
-//  printf("Shortened word is: %s", word);
+  printf("Shortened word is: %s", word);
   return 0;
 }
