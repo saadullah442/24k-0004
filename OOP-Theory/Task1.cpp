cebe1e39fa8d59d59408438b144b2c59cbeb8ddd
Saad Ullah <83773413+saadullah442@users.noreply.github.com>
2025-02-21 13:19:27 +0500

    Add files via upload

--- OOP-Theory/Task1.cpp
+++ OOP-Theory/Task1.cpp
@@ -11,15 +11,19 @@ class Student {
         string studentID;
         string name;
         int age;
-        Skill sportsIntereset[3];
+        Sport *sportsIntereset[3];
         Mentor *mentorAssigned[3];
     public:
         Student(string id, string n, int a): studentID(id), name(n), age(a) {
+            for(int i = 0; i < 3; i++) {
+                sportsIntereset[i] = nullptr;
+                mentorAssigned[i] = nullptr;
+            }
             cout << "Student constructor called\n";
         };
         void registerForMentorship(Mentor &m);
         void viewMentorDetails();
-        void updateSportsInterest(Sport s);
+        void updateSportsInterest(Sport &s);
 
 }
 
@@ -40,18 +44,48 @@ void Student::registerForMentorship(Mentor &m) {
 }
 
 void Studnet::viewMentorDetails() {
-    for(int i = 0;i)
+    cout << "Mentor Details\n";
+    for(int i = 0;i<3;i++) {
+        cout << "MentorID: " << mentorAssigned[i].name << '\t';
+        cout << "Name: " << mentorAssigned[i].name << '\t';
+        cout << "maxLearners: " << mentorAssigned[i].name << endl;
+        cout << "SportExpertise" << endl;
+        for(int j = 0; j < mentorAssigned[i].maxLearners;j++){
+            cout << mentorAssigned[i].sportsExpertise[i].skillName << '\t';
+        }
+        cout << "assignedLearners" << endl;
+        for(int j = 0; j < mentorAssigned[i].maxLearners;j++){
+            cout << mentorAssigned[i].assignedLearner[i].name << '\t';
+        }
+    }
+}
+
+
+void Student::updateSportsInterest(Sport &s) {
+    for(int i = 0; i < 3; i++) {
+        if(sportsIntereset[i] == nullptr) {
+            sportsIntereset[i] = new s;
+        }
+        else {
+            cout << "Cannot add any sports\n";
+        }
+    }
 }
 
 class Mentor {
     string mentorID;
     string name;
     int maxLearners;
-    string sportsExpertise[maxLearners];
+    Skill *sportsExpertise[maxLearners];
     Student *assignedLearners[maxLearners];
     public:
         Mentor(string id, string n, int maxLs): mentorID(id), name(n), maxLearners(maxLs) {
             cout << "Mentor constructor called\n";
+            for(int i = 0;i < maxLearners; i++) {
+                sportsExpertise[i] = nullptr;
+                assignedLearner[i] = nullptr;
+            }
+        
         }
         void assignedLearner(Student &s);
         void removeLearner(Student &s);
